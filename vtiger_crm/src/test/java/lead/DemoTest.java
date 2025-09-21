666e2f37ea51d32f4bcd6f753a9d833b9173f172
Admin <Admin@DESKTOP-FFALLFF>
2025-09-21 21:57:09 +0530

    created new package inside src/test/java please work to gather

---
+++ vtiger_crm/src/test/java/lead/DemoTest.java
@@ -0,0 +1,15 @@
+package lead;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+/**
+ * @author Lead Amit
+ */
+public class DemoTest {
+@Test
+public void createLeadTest(){
+	Reporter.log("run creatLead test",true);
+	
+}
+}
